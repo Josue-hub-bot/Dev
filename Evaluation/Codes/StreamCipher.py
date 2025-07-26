d952ed053407cea07e0b9a211e545c59b06b4a28
Votre nom <your@exemple.com>
2025-07-26 05:56:32 -0400

    changement

---
+++ Evaluation/Codes/StreamCipher.py
@@ -0,0 +1,52 @@
+class KeyStream:
+	"""
+	Classe KeyStream
+	Classe pour générer un flux de clés
+	"""
+	def __init__(self, key=1):
+		"""
+		init (self, key=1)
+		Initialise l'objet clé
+		
+		Paramètres:
+		self : notre objet flux de clé
+		key (int) : la clé partagée,
+		elle est à 1 par défaut
+		
+		Return
+		Notre objet KeyStream
+		"""
+		
+		# Initialise l'objet à la clé
+		self.next = key
+	
+	def rand(self):
+		"""
+		rand(self)
+		Calcul la valeur aléatoire
+		
+		Paramètres:
+		self : notre objet flux de clés
+
+		Return:
+		self.next (int) : la prochaine valeur aléatoire
+		"""
+		
+		# L'équation pour notre LCG
+		# Xnext+1 = (a*Xnext + c) mod m
+		self.next = (1103515245 * self.next + 12345) % 2**31
+		return self.next
+		
+	def get_key_byte(self):
+		"""
+		get_key_byte(self)
+		Crée le flux de clé
+		
+		Paramètres:
+		self : notre objet flux de clé
+		
+		Return:
+		Retourne une clé aléatoire d’un caractère (le mod 256)
+		"""
+		
+		return self.rand() % 256
